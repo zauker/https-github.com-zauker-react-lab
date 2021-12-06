d473631aa57b449f5bba9eab6091586b6f9415ad
zauker <m.menciassi@gmail.com>
2021-12-06 12:36:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-zlxxux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-zlxxux)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { WheaterList } from './components/wheather';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <WheaterList></WheaterList>
+    </div>
+  );
+}
